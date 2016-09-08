4a51751a575ab390bdad9c1d0e665a3fd853d8f7
workspacenew <workspacegithub@gmail.com>
2016-09-09 01:09:55 +0530

    Update

--- ui/auth.js
+++ ui/auth.js
@@ -4,4 +4,6 @@ function onSignIn(googleUser) {
 	console.log('Name: ' + profile.getName());
 	console.log('Image URL: ' + profile.getImageUrl());
 	console.log('Email: ' + profile.getEmail());*/
+	$("#signin").addClass("hidden");
+	$("#apps").removeClass("hidden");
 }
