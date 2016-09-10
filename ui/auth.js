054182a29ec0e29323572592697d7f98452c4251
workspacenew <workspacenew@users.noreply.github.com>
2016-09-10 12:48:10 +0530

    Update

--- ui/auth.js
+++ ui/auth.js
@@ -4,16 +4,7 @@ function onSignIn(googleUser) {
 	console.log('Name: ' + profile.getName());
 	console.log('Image URL: ' + profile.getImageUrl());
 	console.log('Email: ' + profile.getEmail());*/
-}
-
-function appLoad(t){
-	if(t){
-		$("#signin").addClass("hidden");
-		$("#apps").removeClass("hidden");
-		$("body").addClass("appbg");
-	}else{
-		$("#signin").removeClass("hidden");
-		$("#apps").addClass("hidden");
-		$("body").removeClass("appbg");
-	}
+	$("#signin").addClass("hidden");
+	$("#apps").removeClass("hidden");
+	$("body").addClass("appbg");
 }
