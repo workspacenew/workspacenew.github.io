51ac1ed5ad79cb97381ad430f5a422cc87c71aed
workspacenew <workspacenew@users.noreply.github.com>
2016-09-24 23:50:08 +0530

    Update

--- js/ws.js
+++ js/ws.js
@@ -25,6 +25,22 @@ function onSignIn(googleUser) {
 	console.log('Name: ' + profile.getName());
 	console.log('Image URL: ' + profile.getImageUrl());
 	console.log('Email: ' + profile.getEmail());*/
-	$("#signin").addClass("hidden");
-	$("#wsapps").removeClass("hidden");
+	setProgress(80);
+	$.getJSON('https://script.google.com/macros/s/AKfycbxZltCYi59joSZn20WE9EACY8hrkx5_PiZxMSs5cBrZTToZ-eV9/exec/?type=auth&email='+profile.getEmail()).done(function(result){
+		if(result){
+			$("#signin").addClass("hidden"); $("#wsapps").removeClass("hidden");
+		}else{
+			msg('Sorry! Unable to process your request.'); setProgress(100);
+		}
+	}).fail(function(result){
+		msg('Error: Connection problem'); setProgress(100);
+	});
 }
+
+function setProgress(v){
+	
+}
+function msg(t){alert(t);console.log(t);}
+function log(t){console.log(t);}
+function url(t, u){document.title = wsAppTitle+' '+t; history.pushState({}, t, u);}
+function formatDateTime(t){return t.getDate()+'/'+t.getMonth()+'/'+t.getFullYear()+' '+t.getHours()+':'+t.getMinutes();}
