17200d8fd8c8428be263eadffdaeb7779117c0d5
workspacenew <workspacenew@users.noreply.github.com>
2016-10-01 09:55:47 +0530

    Update app.js

--- apps/activity/app.js
+++ apps/activity/app.js
@@ -1,4 +1,4 @@
 var page = '<h2>Activity Stream</h2><p><textarea id="s-p-i1" rows="7" cols="30" style="height:200px"></textarea></p><p><button id="s-p-b1" class="k-primary">Post</button></p>';
 $("#s-p").html(page);
 $("#s-p-i1").kendoEditor({ resizable: {	content: true, toolbar: true }});
-$("#s-p-b1")..kendoButton();
+$("#s-p-b1").kendoButton();
